a0960fb1dda49645e0dffd0e3454618630fb293e
Roberto-Utagawa <utagawaroberto@gmail.com>
2022-02-24 10:58:09 -0300

    02/24/22

--- blackJack.py
+++ blackJack.py
@@ -1,3 +1,4 @@
+from ast import While
 import itertools, random
 
 class CARD(object):
@@ -13,19 +14,27 @@ class DECK(object):
 
     # INSERT A CARD INTO THE DECK #
     def push(self, card):
+        self.amount += 1
         self.cards.append(card)
 
     # REMOVE A CARD FROM THE DECK #
     def pop(self):
+        self.amount -= 1
         return self.cards.pop()
 
     # SHUFFLE DE DECK #
     def deckShuffle(self):
+        while self.amount > 0:
+            self.pop()
+
         temp = list(itertools.product(range(1, 14), ['Ouros', 'Copas', 'Espadas', 'Paus']))
         random.shuffle(temp)
+
         for i in temp:
             carta = CARD(i[0], i[1])
             self.push(carta)
+
+        self.amount = 52
 
 class PLAYER(object):
     def __init__(self):
@@ -33,8 +42,8 @@ class PLAYER(object):
         self.hand = []
         self.balance = 1000
 
-    def push(self, card):
-        self.hand.append(card)
+    def push(self, deck):
+        self.hand.append(deck.pop())
         self.cardsQuantity += 1
 
     def pop(self):
@@ -48,6 +57,7 @@ class GAME(object):
             self.bet = 0
             self.playerBet = []
             self.deck = DECK()
+
         # BLACK JACK RULES #
         def rules(self):
             print('\n\n')
@@ -59,11 +69,14 @@ class GAME(object):
             print('- Each player starts with 1000 balance, if the balance reach 0, the player will be disqualifiqued')
             print('- In case of drawn, the bets will be returned for each player\n')
             print('===================================\n')
+
         # PUSH PLAYERS #
         def playerPush(self, amount):
             for i in range(amount):
                 self.players += 1
                 self.player.append(PLAYER())
+                self.playerBet.append(0)
+
         # DEFINE THE BET #
         def betInsert(self, player, bet):
             if bet == 0 or bet > self.player[player].balance:
@@ -71,23 +84,61 @@ class GAME(object):
             self.bet += bet
             self.playerBet[player] = bet
             self.player[player].balance -= bet
+
         # PAY THE BET TO THE WINNER #
         def betPay(self, player):
             self.player[player].balance += self.bet
             self.bet = 0
+
         # RESET THE BET #
         def betClear(self):
             self.bet = 0
             for i in range(self.players):
                 self.playerBet[i] = 0
+
         # TAKE CARDS IF POSSIBLE#
         def playerTakeCard(self, index):
             if self.playerBet[index] > 0:
                 take = 1
                 while take == 1:
-                    take = int(input('Player {}, you have {} cards, type 1 to take more cards, or 0 to stop: ').format(index, self.player[index].cardsQuantity))
+                    take = int(input('Player {}, you have {} cards, type 1 to take more cards, or 0 to stop: '.format(index, self.player[index].cardsQuantity)))
                     if take == 1:
-                        self.players[index].push(self.deck.pop)
+                        self.player[index].push(self.deck)
+
+        # GIVE BACK THE BET TO EACH PLAYER #
+        def betBack(self):
+            for i in range(self.players):
+                self.player[i].balance += self.playerBet[i]
+            self.bet = 0
+
+        def betsReset(self):
+            for i in range(self.players):
+                self.playerBet[i] = 0
+
+        def IsPossible(self):
+            players = 0
+            for i in range(self.players):
+                if self.player[i].balance > 0:
+                    players += 1
+            if players > 1:
+                return True
+            return False
+        
+        def winner(self):
+            bigger = -1
+            index = -1
+            draw = False
+            for i in range(self.players):
+                if self.player[i].balance > bigger:
+                    bigger = self.player[i].balance
+                    index = i
+                    draw = False
+                elif self.player[i].balance == bigger:
+                    draw = True
+            if draw:
+                print('ITS A DRAW !!!!')
+            else:
+                print('CONGRATULATIONS PLAYER {}, YOU WIN!!!'.format(index))
 
 def blackJack():
     game = GAME()
@@ -95,100 +146,39 @@ def blackJack():
     game.playerPush(int(input('Select the number of players: ')))
     continue_ = True
 
-    while #possivel jogar? and continue_ == True:
+    while continue_ == True and game.IsPossible():
         for i in range(game.players):
             if game.player[i].balance > 0:
-                bet = int(input('Player {}, choose your bet: ').format(i))
+                print('Player {}, you balance is {}'.format(i, game.player[i].balance))
+                bet = int(input('Player {}, choose your bet: '.format(i)))
                 game.betInsert(i, bet)
         bigger = -1
+        index = -1
+        draw = 0
         for i in range(game.players):
+            sum = 0
             game.playerTakeCard(i)
-            #VERIFICAR MAO
-            #COLOCAR O MAIOR, SE HOUVER EMPATE DEVOVLER BETS
-            # RESETAR BARALHO
-            # RESETAR BETS
-            # RESETAR CARTAS PLAYERS
+            while game.player[i].cardsQuantity > 0:
+                card = game.player[i].pop()
+                sum += card.value
+            if sum <= 21 and sum > bigger:
+                bigger = sum
+                index = i
+                draw = 0
+            elif sum == bigger:
+                draw = 1
+            print('Player {}, the sum is: {}'.format(i, sum))
+        if draw == 1 or index == -1:
+            print('Its a draw')
+            game.betBack()
+        else:
+            print('Player {} win this round'.format(index))
+            game.betPay(index)
+
+        game.deck.deckShuffle()
+        game.betsReset()
         continue_ = int(input('Press 1 to continue playing, 0 to stop: '))
 
+    game.winner()
+
 blackJack() 
-
-
-
-
-'''''
-# INSERT THE CARDS INTO THE DECK #
-
-# CREATE THE PLAYERS #
-def players():
-    qnt = int(input('\nDigite a quantidade de jogadores: '))
-    jogadores = []
-    for i in range(0, qnt):
-        jogador = JOGADOR()
-        jogadores.append(jogador)
-    print('\n===================================')
-    return jogadores
-
-# TAKE CARDS TO EACH PLAYER #
-def take_cards(jogadores, deck):
-    for i in range(len(jogadores)):
-        print('\nJOGADOR {}:'.format(i))
-        continue_ = True
-        while continue_ == True:
-            print('\nJogador {}, voce tem um total de {} cartas'.format(i, jogadores[i].quantidade))
-            continue_ = int(input('Digite 1 para pegar uma carta ou 0 para sair: '))
-
-            if continue_ == True:
-                jogadores[i].quantidade += 1
-                jogadores[i].push(deck.pop())
-        print('\n===================================')
-
-# CHECK IF IS POSSIBLE PLAY #
-def check_balance(jogadores):
-    for i in range(len(jogadores)):
-        if jogadores[i].saldo  <= 0:
-            return False
-    return True
-
-def bet(jogadores):
-    bet_ = 0
-    for i in range(len(jogadores)):
-        bet1 = input(int('Player: {}, select the bet: '))
-        if bet1 > jogadores[i].saldo or bet <= 0:
-            bet1 = jogadores[i].saldo
-        jogadores[i].saldo -= bet1
-        bet_ += bet1
-    return bet_
-
-def check_game(jogadores):
-    card = -1
-    bigger = -1
-    index_b = -1
-
-    for i in range(len(jogadores)):
-        total = 0
-        while jogadores[i].quantidade != 0:
-            card = jogadores[i].pop()
-            total += card.valor
-            jogadores[i].quantidade -= 1
-        if total <= 21 and total > bigger:
-            index_b = i
-            bigger = total
-
-    return index_b
-
-def main():
-    print('\n\n============ BLACK JACK ===========')
-    jogadores = players()
-
-    while check_balance(jogadores):
-        index = -1
-        deck = BARALHO()
-        deck_push(deck)
-        bet_ = bet(jogadores)
-        take_cards(jogadores, deck)
-        index = check_game(jogadores)
-        jogadores[index].saldo += bet
-    # anunciar quem venceu
-
-main()
-'''''
